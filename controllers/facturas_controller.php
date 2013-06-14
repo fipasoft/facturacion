97e91bc9b3ba781050dcb034ba78aa9ab3b0a763
mimex <mimex@fipasoft.mx>
2013-06-14 11:03:55 -0500

    RFT: Simplificación del encabezado

--- controllers/facturas_controller.php
+++ controllers/facturas_controller.php
@@ -1,12 +1,10 @@
 <?php
-// facturacion, Creado el 17/11/2011
 /**
  * Facturas
  *
  * @package    Controladores
- * @author     mimeks <mimex@fipasoft.com.mx>
- * @copyright  2011 FiPa Software (contacto at fipasoft.com.mx)
- * @license    http://www.gnu.org/licenses/gpl.html  GNU General Public License (GPL)
+ * @copyright  FiPa Software (contacto at fipasoft.com.mx)
+ * @license    http://www.gnu.org/licenses/gpl.html  GNU GPL
  *
  */
 class FacturasController extends ApplicationController
